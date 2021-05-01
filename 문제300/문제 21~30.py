a174ed16e986521c184ee186ae7ac107e7904819
krsthg <ypseoha@naver.com>
2021-05-01 17:21:24 +0900

    문제 21~30

--- 문제300/문제 21~30.py
+++ 문제300/문제 21~30.py
@@ -1,20 +1,23 @@
-#021
+#021: 안덱싱[문자순서]: 0번시작
 letters='python'
 print(letters[0], letters[2])
 
-#022
+#022: 슬라이싱[시작번호: 끝번호: 단위]
+    #시작번호부터 끝번호 전까지의 문자출력
 license_plate = "24가 2210"
 print(license_plate[4:8])
+print(license_plate[-4:]) #뒤에서부터 4개
 
 #023
 string ="홀짝홀짝홀짝"
 print(string[::2])
+print(string[0:6:2]) #0부터 6까지 2단위로 이동[0 2 4]
 
-#024
+#024: 슬라이싱 반대로
 string = "PYTHON"
 print(string[: :-1])
 
-#025
+#025: replace("기존문자", "새로운문자") 교체함수
 phone = "010-1111-2222"
 p= phone.replace("-"," ")
 print(p)
