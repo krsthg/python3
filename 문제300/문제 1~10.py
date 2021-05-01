b92c4f8777d7402846ac12ba11d960d619b7bf56
krsthg <ypseoha@naver.com>
2021-05-01 16:17:52 +0900

    python

--- 문제300/문제 1~10.py
+++ 문제300/문제 1~10.py
@@ -10,21 +10,22 @@ print('신씨가 소리질렀다. "도둑이야"')
 
 #004
 print('"C:\Windows"')
-
+    #엔터위에 원회기호:\
 #005
 print("안녕하세요\n만나서\t\t반갑습니다.")
+    #\n: 줄바꿈 \t: 탭[들여쓰기]
 
-#006
+#006: ,[쉼표] 구분짓기
 print("오늘은","일요일")
 
-#007
+#007: ;[세미콜론]
 print("naver","kakao","sk","samsung", sep=';')
 
 #008
 print("naver","kakao","sk","samsung", sep='/')
 
-#009
-print("first");print("second")
+#009: end='' 끝마침을 공백으로 함
+print("first", end='');print("second")
 
 #010
-print(5/3)
+print("5/3")
