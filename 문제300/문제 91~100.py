faebba82a4e2e7d93df02627d067b94e45263dc0
krsthg <ypseoha@naver.com>
2021-05-09 17:12:34 +0900

    문제 91~100

--- 문제300/문제 91~100.py
+++ 문제300/문제 91~100.py
@@ -48,3 +48,4 @@ date = ['09/05', '09/06', '09/07', '09/08', '09/09']
 close_price = [10500,10300,10100,10800,11000]
 close_table = dict(zip(date, close_price))
 print(close_table)
+    #date 리스트와 close_price 리스트를 딕셔너리로 변환
