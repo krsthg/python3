a5952ef059cf2e36daa23420a751f198c75c2cc1
krsthg <ypseoha@naver.com>
2021-05-16 15:34:53 +0900

    2020-05-16

---
+++ 문제300/문제 151~160.py
@@ -0,0 +1,56 @@
+#151
+list = [3,-20,-3,44]
+for i in list:
+    if i<0:
+        print(i)
+
+#152
+list = [3,100,23,44]
+for i in list:
+    if i%3==0:
+        print(i)
+
+#153
+list = [13, 21,12,14,30,18]
+for i in list:
+    if i%3==0 and i<20 :
+        print(i)
+
+#154
+list = ["I","study","python","language","!"]
+for i in list[1:4]:
+    print(i)
+
+#155
+list = ['A',"b","c","D"]
+for i in list:
+    if i.isupper()== True:
+        print(i)
+
+#156
+for i in list:
+    if i.isupper() == False:
+        print(i)
+
+#157
+list = ['dog','cat','parrot']
+for i in list:
+    print(i[0].upper()+i[1:])
+
+#158
+list = ['hello.py','ex01.py','define.h','run.py']
+for i in list:
+    list = i.split(".")
+    print(list[0])
+
+# 159
+list = ['intra.h', 'intra.c', 'define.h', 'run.py']
+for i in list:
+    if i[-1] == "h":
+        print(i)
+
+# 160
+list = ['intra.h', 'intra.c', 'define.h', 'run.py']
+for i in list:
+    if i[-1] == "h" or i[-1] == "c":
+        print(i)
---
+++ 문제300/문제 161~170.py
@@ -0,0 +1,49 @@
+#161
+for i in range(100):
+    print(i)
+
+#162
+for i in range(2002, 2051, 4):
+    print(i)
+
+#163
+for i in range(1, 31):
+    if i %3 == 0:
+        print(i)
+
+#164
+for i in range(99, -1 , -1):
+    print(i)
+
+#165
+for i in range(10):
+    print(i/10)
+
+#166
+for i in range(1,10):
+    print(3, "X", i, "=", 3*i)
+
+#167
+for i in range(1, 10):
+    if i % 2 == 1:
+        print(3, "X", i, "=", 3*i)
+
+#168
+n = 0
+for i in range(1, 11):
+    n += i
+print(n)
+
+#169
+n = 0
+for i in range(1,11):
+    if i %2 == 1:
+        n += i
+print(n)
+
+#170
+n = 1
+for i in range(1, 11):
+    n *= i
+print(n)
+
