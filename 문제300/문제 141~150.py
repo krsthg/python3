c049c94d3733d2139783e16041a8dbe35a2b8020
krsthg <ypseoha@naver.com>
2021-05-16 15:10:55 +0900

    2020-05-16

---
+++ 문제300/문제 141~150.py
@@ -0,0 +1,45 @@
+#141
+list = [100,200,300]
+for i in list:
+    print(i+10)
+
+#142
+list = ["김밥","라면","튀김"]
+for i in list:
+    print("오늘의 메뉴: ", i)
+
+#143
+list = ["SK하이닉스","삼성전자","LG전자"]
+for i in list:
+    print(len(i))
+
+#144
+list = ['dog','cat','parrot']
+for i in list:
+    print(i, len(i))
+
+#145
+for i in list:
+    print(i[0])
+
+#146
+list=[1,2,3]
+for i in list:
+    print('3',"x", i)
+
+#147
+for i in list:
+    print('3',"x", i,'=',3*i)
+
+#148
+list = ['가','나','다','라']
+for i in list[1:]:
+    print(i)
+
+#149
+for i in list[::2]:
+    print(i)
+
+#150
+for i in list[::-1]:
+    print(i)
