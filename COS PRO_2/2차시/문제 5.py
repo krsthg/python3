40958b9642c3f5064cb7e90bd6c2e15a03b945f6
현예담 <happy-dam@naver.com>
2021-06-05 17:16:11 +0900

    2021-06-05

--- COS PRO_2/2차시/문제 5.py
+++ COS PRO_2/2차시/문제 5.py
@@ -2,16 +2,23 @@
 def solution(attack, recovery,hp):
     count = 0
     while (True):
-        count += 1
-        hp -= 1
-        if hp <= 1:
-            count += 1
-        hp += 1
+        count += 1 #공격횟수 1증가
+        hp -= attack #몬스터체력 -= 공격력
+        if hp <= 0: #만약에 몬스터체력 <= 0 보다 작을때
+            break #반복문 종료 [공격 종료]
+        hp += recovery #몬스터체력
     return count
 
-attack = 30
-recovery = 10
-hp =60
+attack = 30     #캐릭터 공격력
+recovery = 10   #몬스터의 회복체력
+hp =60          #몬스터의 초기체력
 ret = solution(attack,recovery,hp)
 
 print("solution 함수의 결과:", ret)
+
+#몬스터 60
+#count = 1    #공격 -30 => 몬스터 30
+    #회복 +10 => 몬스터 40
+#count = 2        #공격 -30 => 몬스터 10
+    #회복 +10 => 몬스터 20
+#count = 2        #공격 -30 => 몬스터 -10[몬스터체력이 <= 0:종료]
--- COS PRO_2/2차시/문제 6.py
+++ COS PRO_2/2차시/문제 6.py
@@ -2,13 +2,28 @@
 def solution(floors):
     dist  = 0
     length = len(floors)
-    for i in range(len(floors)+1):
-        if @@@:
+    for i in range(1, length): #1~5전까지
+        if floors[i]>floors[i-1]: #현재층과 앞전층을 비교했을때 현재층이 더 크면
             dist += floors[i] - floors[i-1]
-        else:
+        else:   #앞전층이 더 크면
             dist += floors[i-1]- floors[i]
+    return dist
+        #층 [1,2,5,4,2]
+            #2>1
+                #2-1 => dist = 1
+            #5>2
+                #5-2 => dist = 3
+            #4>5[x]
+                #5-4 => dist = 1
+            # 2>4[x]
+                # 4-2 => dist = 2
 
 floors = [1,2,5,4,2]
+        #층과 층 사이의 거리는 1
+        #1 -> 2 [1]
+        #2 -> 5 [3]
+        #5 -> 4 [1]
+        #4 -> 2 [2] => 총 7층
 ret = solution(floors)
 
 print("solution 함수의 결과:", ret)
