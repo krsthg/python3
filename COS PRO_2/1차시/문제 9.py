72a0be2e4b3417d070763af57b31d60bacd1ad70
현예담 <happy-dam@naver.com>
2021-06-05 17:23:53 +0900

    2021-06-05

--- COS PRO_2/1차시/문제 9.py
+++ COS PRO_2/1차시/문제 9.py
@@ -3,7 +3,7 @@ def soultion(characters):
     result = '' #공백
     # result += characters[0] #첫문자르 result에 저장
     for i in range(len(characters)): #하나씩 비교
-        #0[첫번째] 젱힌 1[두번째] 바교
+        #0[첫번째] 글자 1[두번째] 비교
         if characters[i-1] != characters[i]:
             result += characters[i] #첫문자를 또 저장하면 오류
 
