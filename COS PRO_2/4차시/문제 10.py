229910a278ecdb2402dd3f4d634fb7c9a7b9c461
현예담 <happy-dam@naver.com>
2021-06-19 15:06:15 +0900

    20201-06-19

--- COS PRO_2/4차시/문제 10.py
+++ COS PRO_2/4차시/문제 10.py
@@ -1,15 +1,14 @@
-# 다음과 같이 import를 사용할 수 있습니다.
 # import math
 
 def solution(scores, cutline):
-    # 여기에 코드를 작성해주세요.
     answer = 0
+    for score in scores: #점수리스트를 하나씩 점수에 넣기
+        if score >= cutline: #해당 점수가 커트라인보다 높으면
+            answer +=1 #합격자수 1 씩 증가
     return answer
 
-# 아래는 테스트케이스 출력을 해보기 위한 코드입니다.
-scores = [80, 90, 55, 60, 59]
-cutline = 60
+scores = [80, 90, 55, 60, 59] #학생들 점수 리스트
+cutline = 60 #커트라인[합격기준]
 ret = solution(scores, cutline)
-
-#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+              #점수리스트, 커트라인
 print("solution 함수의 반환 값은", ret, "입니다.")
--- COS PRO_2/4차시/문제 9.py
+++ COS PRO_2/4차시/문제 9.py
@@ -1,14 +1,25 @@
-#다음과 같이 import를 사용할 수 있습니다.
 #import math
 
 def solution(height):
-    #여기에 코드를 작성해주세요.
     count = 0
+    dx = [-1, 1, 0, 0] #가로축
+    dy = [0, 0, -1, 1] #세로축
+    for i in range(4): #4번 반복
+        for j in range(4): #4번 반복
+            d = True
+            for k in range(4): #4번 반복
+                if 0 <=i+dx[k] and i+ dx[k] <4 and 0<= j+ dy[k] and j+ dy[k]<4:
+                    if height[i+dx[k]][j+dy[k]] <= height[i][j]:
+                        d = False
+            if d:
+                count+=1
     return count
 
-#아래는 테스트케이스 출력을 해보기 위한 코드입니다.
-height = [[3, 6, 2, 8], [7, 3, 4, 2], [8, 6, 7, 3], [5, 3, 2, 9]]
+height = [[3, 6, 2, 8], #3, 2
+          [7, 3, 4, 2], #3, 2
+          [8, 6, 7, 3],
+          [5, 3, 2, 9]] #2
+#현재 수 기준으로 오른쪽[동] 왼쪽[서] 아래[남] 위[북] 수가 더 클 경우 => 위험지역
+#2차원 리스트[[첫번째 줄], [두번째 줄], [세번째 줄], [네번째 줄]]
 ret = solution(height)
-
-#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret, "입니다.")
