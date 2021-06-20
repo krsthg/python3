4ccde9a36be9e64a52093e4694b99d04e6b72530
현예담 <happy-dam@naver.com>
2021-06-20 15:13:42 +0900

    20201-06-19

--- COS PRO_2/5차시/문제 7.py
+++ COS PRO_2/5차시/문제 7.py
@@ -1,17 +1,18 @@
 def solution(stuffs):
     answer = 0
     small_counter, general_counter = 0, 0
-    for s in stuffs:
-        if s > 3:
-            general_counter += s
-        else:
-            small_counter += s
+    for s in stuffs: #손님들의 물건 구매 수
+        if s > 3: #3초과
+            general_counter += s #일반 계산대 +1
+        else: #아니면
+            small_counter += s #소량 계산대 +1
+    #if small_counter > general_counter: => 오답
     if small_counter > general_counter:
         answer = small_counter
     else:
         answer = general_counter
     return answer
 
-stuffs = [5, 3, 4, 2, 3, 2]
+stuffs = [5, 3, 4, 2, 3, 2] #손님들의 물건 구매수
 ret = solution(stuffs)
 print("solution 함수의 반환 값은 ", ret, " 입니다.")
--- COS PRO_2/5차시/문제 8.py
+++ COS PRO_2/5차시/문제 8.py
@@ -1,11 +1,16 @@
 def solution(usage):
     answer = 0
-    if usage > 30:
+    if usage > 30: #상수도 30 초과이면
+        # answer = 20 * 430 + 10 * 570 + (usage - 20) * 840
+        #          20톤 가격, 10톤 가격, 20톤을 뺀 * 840
         answer = 20 * 430 + 10 * 570 + (usage - 30) * 840
-    elif usage > 20:
+                 #1단계 가격, 2단계 가격, 3단계 가격
+    elif usage > 20: #상수도 20 초과이면
         answer = 20 * 430 + (usage - 20) * 570
-    else:
+                #1단계 가격, 2단계 가격
+    else: #그 외 상수도 20이하
         answer = usage * 430
+                #1단계 가격
     return answer
 
 usage = 35
