543cb74d888c7941e856cdf6dcb3d532d96b0a06
현예담 <happy-dam@naver.com>
2021-06-27 17:39:45 +0900

    20201-06-27

--- COS PRO_2/1차시 모의고사/문제 10.py
+++ COS PRO_2/1차시 모의고사/문제 10.py
@@ -7,7 +7,7 @@ def solution(arr, k): #arr: 2차원 리스트     #k: k번째 작은수
     n = len(arr) #n: 배열의 세로길이
     for h in range(n):
         for w in range(4): #4는 배열의 가로길이
-            list.append(list[h][w])
+            list.append(arr[h][w])
     list.sort() #정렬 => sort: 오름차순으로 정렬
     answer = list[k-1] #인덱스는 0번부터 시작
     return answer
