2d0a28428ddb5155ac61880cc0d359946e0fec39
현예담 <happy-dam@naver.com>
2021-06-27 17:09:03 +0900

    20201-06-27

--- COS PRO_2/1차시 모의고사/문제 1.py
+++ COS PRO_2/1차시 모의고사/문제 1.py
@@ -1,11 +1,16 @@
-def func_a(k):
-    sum = 0
+#1~ ㅡ-1까지 누적합계
+#1단계~2단계의 결과값
+def func_a(k): #m->k        n-1->k
+    sum = 0 #누적합계 변수
     for i in range(k+1):
-        sum += i
+    #for 변수 in range(~전까지):
+        #i는 1부터 k전까지 반복
+        #i는 1부터 k+1까지 반복
+        sum += i #i를 sum에 저장
     return sum
 
 def solution(n, m):
-    sum_to_m = func_a(m)
-    sum_to_n = func_a(n-1)
-    answer = sum_to_m - sum_to_n
+    sum_to_m = func_a(m) #1단계
+    sum_to_n = func_a(n-1) #2단계
+    answer = sum_to_m - sum_to_n #3단계
     return answer
