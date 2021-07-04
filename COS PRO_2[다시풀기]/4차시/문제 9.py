86a041e5ae42b35365d9a33929907de29957e33e
현예담 <happy-dam@naver.com>
2021-07-04 16:05:27 +0900

    20201-07-04

--- COS PRO_2[다시풀기]/4차시/문제 9.py
+++ COS PRO_2[다시풀기]/4차시/문제 9.py
@@ -1,18 +1,20 @@
 #다음과 같이 import를 사용할 수 있습니다.
 #import math
 
-def solution(words, word):
+def solution(height):
     #여기에 코드를 작성해주세요.
     count = 0
-    for i in words:
-        if i != "CODE":
+    w = [0,0,0,0]
+    h = [0,0,0,0]
+    for i in range(4):
+        for j in range(4):
 
+            count+=1
     return count
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
-words = ["CODE", "COED", "CDEO"]
-word = "CODE"
-ret = solution(words, word)
+height = [[3, 6, 2, 8], [7, 3, 4, 2], [8, 6, 7, 3], [5, 3, 2, 9]]
+ret = solution(height)
 
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret, "입니다.")
