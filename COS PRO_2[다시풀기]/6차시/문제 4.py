3e580c107782233f670c3cd162135ea6d9fbbd08
현예담 <happy-dam@naver.com>
2021-07-10 17:07:16 +0900

    20201-07-10

--- COS PRO_2[다시풀기]/6차시/문제 4.py
+++ COS PRO_2[다시풀기]/6차시/문제 4.py
@@ -3,7 +3,8 @@
 
 def solution(cards):
     #여기에 코드를 작성해주세요.
-    answer = 0
+    answer = [0,0,0]
+    j = 0
     for i in cards:
         if i[0] == "black":
             answer[0] +=1
@@ -11,7 +12,12 @@ def solution(cards):
             answer[1] +=1
         elif i[0] == "blue":
             answer[2] += 1
-    return answer
+        j += int(i[1])
+    if answer[0] == 3 or answer [1] == 3 or answer[2] == 3:
+        j *=3
+    elif answer[0] == 2 or answer [1] == 2 or answer[2] == 2:
+        j *= 2
+    return j
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
 cards1 = [["blue", "2"], ["red", "5"], ["black", "3"]]
