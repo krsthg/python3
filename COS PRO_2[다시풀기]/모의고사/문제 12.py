9ffc9d80bd49613a6b4e07ab430f7d58962c9e70
현예담 <happy-dam@naver.com>
2021-07-11 17:02:04 +0900

    20201-07-11

--- COS PRO_2[다시풀기]/모의고사/문제 12.py
+++ COS PRO_2[다시풀기]/모의고사/문제 12.py
@@ -3,11 +3,11 @@ def solution(cards):
     answer = [0,0,0]
     j = 0
     for i in cards:
-        if i[0] == "R":
+        if i[0] == "blue":
             answer[0] +=1
-        elif i[0] == "G":
+        elif i[0] == "black":
             answer[1] +=1
-        elif i[0] == "B":
+        elif i[0] == "red":
             answer[2] += 1
         j += int(i[1])
     if answer[0] == 3 or answer [1] == 3 or answer[2] == 3:
@@ -16,7 +16,7 @@ def solution(cards):
         j *= 2
     return j
 
-cards1 = [["R","5"],["G","2"], ["B","3"]]
-cards2 = [["G","5"],["G","2"], ["B","3"]]
+cards1 = [["blue","2"],["red","5"], ["black","3"]]
+cards2 = [["blue","5"],["blue","2"], ["black","3"]]
 print("solution 결과:", solution(cards1))
 print("solution 결과:", solution(cards2))
